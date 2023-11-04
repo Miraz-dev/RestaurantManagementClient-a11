bb9148a89eb438828a4db5603b6e69c342f11d86
Miraz Farid <miraz.officialx@gmail.com>
2023-11-05 02:02:08 +0600

    Configured the /login and /register routes. Also, created and designed the respective authentication pages

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home/Home";
 import Root from "../layout/Root";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
 const router = createBrowserRouter([
     {
@@ -16,6 +18,14 @@ const router = createBrowserRouter([
             },
             {
                 path: "/blog"
+            },
+            {
+                path: "/login",
+                element: <Login></Login>
+            },
+            {
+                path: "/register",
+                element: <Register></Register>
             }
         ]
     }
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBY_uGb9I9VEhCHcFCFmbgld3N8-hSuyY",
+  authDomain: "restaurant-management-d0c16.firebaseapp.com",
+  projectId: "restaurant-management-d0c16",
+  storageBucket: "restaurant-management-d0c16.appspot.com",
+  messagingSenderId: "926592259741",
+  appId: "1:926592259741:web:c598e221c3dc9345876674"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
